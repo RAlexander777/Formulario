00fa80a2a5bd4822df1f80ff4bef8751447b4b97
RAlexander777 <recterritory@gmail.com>
2020-07-17 11:25:37 -0500

    segundo commit

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+function mostraralerta()
+{       
+    alert('hizo clic!');
+}     
+function hacerclic()
+{       
+    var elemento=document.getElementsByTagName('a')[0];
+    elemento.addEventListener(‘click’, mostraralerta, false);
+}     
+
+window.addEventListener(‘load’, hacerclic, false); 
